b65c5a6a69ed48ef1e10b668586d9459b4977e7e
Felipe da Costa Coqueiro <felipe.coqueiro@usp.br>
2024-07-12 10:44:02 -0300

    fatorando a main

--- main.c
+++ main.c
@@ -19,31 +19,34 @@ int main(void){
     char nomeArquivoBinario[50];
     char nomeArquivoIndex[50];
 
-    scanf("%d", &opcao); // Determina qual funcionalidade irá executar
+    scanf("%d", &opcao); // Escolhe a operação a ser realizada
 
     switch(opcao){
-        case 1: // Funcionalidade 1 (cria arquivo binário)
+        case 1: // Geração de arquivo binário a partir de CSV
             scanf("%s %s", nomeArquivoCSV, nomeArquivoBinario);
             criarArquivoBinario(nomeArquivoCSV, nomeArquivoBinario);
             binarioNaTela(nomeArquivoBinario);
             break;
-        case 2: // Funcionalidade 2 (lista os registros)
+        case 2: // Apresentação de registros
             scanf("%s", nomeArquivoBinario);
             listarRegistros(nomeArquivoBinario);
             break;
-        case 3: // Funcionalidade 3 (faz a busca por campos)
+        case 3: // Pesquisa de registros por campos
             scanf("%s %d", nomeArquivoBinario, &numBuscas);
             buscarRegistros(nomeArquivoBinario, numBuscas);
             break;
-        case 4: // Funcionalidade 4 (cria arquivo de index)
+        case 4: // Criação de arquivo de índice
             scanf("%s %s", nomeArquivoBinario, nomeArquivoIndex);
+
+            // Se o index é criado com sucesso
             if (criarIndex(nomeArquivoBinario, nomeArquivoIndex, opcao)) {
+                // Imprime a o binário na tela
                 binarioNaTela(nomeArquivoIndex);
             }
             break;
-        case 5: { // Funcionalidade 5 (remoção lógica do arquivo de dados)
+        case 5: { // Remoção lógica de registros
             scanf("%s %s %d", nomeArquivoBinario, nomeArquivoIndex, &numRemocoes);
-            // abertura do arquivo
+            // Abrindo o arquivo
             FILE* arquivoBinario = fopen(nomeArquivoBinario, "rb+");
             FILE* arquivoIndex = fopen(nomeArquivoIndex, "rb+");
 
@@ -53,12 +56,13 @@ int main(void){
             }
 
             if(arquivoIndex == NULL) {
-                // Caso o arquivo de índice não exista, cria um novo
+                // Cria um novo arquivo de índice se não existir
                 arquivoIndex = fopen(nomeArquivoIndex, "wb");
             }
 
             int numCampos;
 
+            // Faz a remoção dos registros
             for(int i = 0; i < numRemocoes; i++) {
                 scanf("%d", &numCampos);
                 remover(arquivoBinario, arquivoIndex, numCampos);
@@ -66,7 +70,7 @@ int main(void){
 
             criarIndex(nomeArquivoBinario, nomeArquivoIndex, opcao);
 
-            // Fechar os arquivos
+            // Encerrando os arquivos
             fclose(arquivoBinario);
             fclose(arquivoIndex);
 
@@ -75,9 +79,9 @@ int main(void){
 
             break;
         }
-        case 6: {// Funcionalidade 6 (inserção de novos registros)
+        case 6: {// Inserção de novos registros
             scanf("%s %s %d", nomeArquivoBinario, nomeArquivoIndex, &numInsercoes);
-            // Abertura do arquivo
+            // Abrindo o arquivo
             FILE* arquivoBinario = fopen(nomeArquivoBinario, "rb+");
             FILE* arquivoIndex = fopen(nomeArquivoIndex, "rb+");
 
@@ -87,33 +91,35 @@ int main(void){
             }
 
             if(arquivoIndex == NULL){
-                // Caso o arquivo de índice não exista, cria um novo
+                // Cria um novo arquivo de índice se não existir
                 arquivoIndex = fopen(nomeArquivoIndex, "wb");
                 criarIndex(nomeArquivoBinario, nomeArquivoIndex, opcao);
 
             }
 
+            // Insere os dados
             for(int i = 0; i < numInsercoes; i++) {
                 inserir(arquivoBinario);
             }
 
-            // Apagar o arquivo de índice
+            // Limpa o arquivo de índice
             fclose(arquivoIndex);
             arquivoIndex = fopen(nomeArquivoIndex, "wb");
 
-            // Recria o arquivo binario de index
+            // Recria o arquivo de índice
             criarIndex(nomeArquivoBinario, nomeArquivoIndex, opcao);
 
-            // Fechar os arquivos
+            // Encerrando os arquivos
             fclose(arquivoBinario);
             fclose(arquivoIndex);
 
+            // Imprime o binário na tela
             binarioNaTela(nomeArquivoBinario);
             binarioNaTela(nomeArquivoIndex);
 
             break;
         }
-        case 7: { // Funcionalidade 7 (Cria um arquivo de índice árvore-B)
+        case 7: { // Criação de arquivo de índice tipo árvore-B
             scanf("%s %s", nomeArquivoBinario, nomeArquivoIndex);
 
             if(criarArquivoArvoreB(nomeArquivoBinario, nomeArquivoIndex)) {
@@ -121,61 +127,53 @@ int main(void){
             }
             break;
         }
-        case 8: { // Funcionalidade 8 (Recuperação de dados usando índice árvore-B)
-            scanf("%s %s %d", nomeArquivoBinario, nomeArquivoIndex, &numBuscas); // Lê o nome do arquivo binário
+        case 8: { // Recuperação de dados via índice árvore-B
+            scanf("%s %s %d", nomeArquivoBinario, nomeArquivoIndex, &numBuscas); // Recebe o nome do arquivo binário
 
-            FILE *arquivoBinario = fopen(nomeArquivoBinario, "rb"); // Abre o arquivo binário no modo leitura
+            FILE *arquivoBinario = fopen(nomeArquivoBinario, "rb"); // Abre o arquivo binário para leitura
 
             if(arquivoBinario == NULL) {
-                printf("Falha no processamento do arquivo.\n"); // Verifica se ocorreu um erro ao abrir o arquivo
+                printf("Falha no processamento do arquivo.\n"); // Verifica erro na abertura do arquivo
                 return 0;
             }
 
-            CABECALHO *cabecalhoBin = retornaCabecalhoBinario(arquivoBinario); // Lê o cabeçalho do arquivo binário
+            CABECALHO *cabecalhoBin = retornaCabecalhoBinario(arquivoBinario); // Obtém o cabeçalho do arquivo
 
             if(getStatus(cabecalhoBin) == '0') {
-                printf("Falha no processamento do arquivo.\n"); // Verifica se o status do cabeçalho é inválido
-                limpaCabecalho(cabecalhoBin); // Libera a memória do cabeçalho
+                printf("Falha no processamento do arquivo.\n"); // Checa a validade do status do cabeçalho
+                limpaCabecalho(cabecalhoBin); // Libera recursos do cabeçalho
                 fclose(arquivoBinario); // Fecha o arquivo
                 return(0);
             }
 
-            limpaCabecalho(cabecalhoBin); // Libera a memória do cabeçalho
+            limpaCabecalho(cabecalhoBin); // Libera recursos do cabeçalho
 
             for(int i=0; i<numBuscas; i++) {
-                scanf("\nid %d", &id); // Lê o id a ser buscado
-                buscaIdArvore(id, arquivoBinario, nomeArquivoIndex, i, 0); // Chama a função para imprimir o registro correspondente ao id na árvore B
+                scanf("\nid %d", &id); // Captura o id para busca
+                buscaIdArvore(id, arquivoBinario, nomeArquivoIndex, i, 0); // Executa a busca do registro via id na árvore B
             }
-            fclose(arquivoBinario); // Fecha o arquivo binário
+            fclose(arquivoBinario); // Fecha o arquivo
             break;
         }
-        case 9: { // Funcionalidade 9 (Recuperação de dados usando qualquer campo de busca)
-            char arquivoBin[50];
-            scanf("%s", arquivoBin); // Lê o nome do arquivo binário
+        case 9: { // Busca de dados por qualquer campo usando índice
+            scanf("%s %s", nomeArquivoBinario, nomeArquivoIndex); // Recebe o nome dos arquivos
 
-            char arquivoArvB[50];
-            scanf("%s", arquivoArvB); // Lê o nome do arquivo da árvore B
+            imprimeRegistrosBuscados(nomeArquivoBinario, 1, nomeArquivoIndex); // Exibe registros buscados com a árvore B
 
-            imprimeRegistrosBuscados(arquivoBin, 1, arquivoArvB); // Chama a função para imprimir registros buscados com a árvore B
             break;
         }
-        case 10: { // Funcionalidade 10 (Inserção de novos registros usando índice árvore-B)
-            char arquivoBin[50];
-            scanf("%s", arquivoBin); // Lê o nome do arquivo binário
+        case 10: { // Inserção de registros via índice árvore-B
+            // Recebe o nome do arquivo binário, index e número de buscas
+            scanf("%s %s %d", nomeArquivoBinario, nomeArquivoIndex, &numBuscas);
 
-            char arquivoArvB[50];
-            scanf("%s", arquivoArvB); // Lê o nome do arquivo da árvore B
-            
-            int numOperacoes;
-            scanf("%d", &numOperacoes);
+            // Realiza as inserções nos arquivos de dados e de árvore B
+            bool inseriu = inserirNovoDadoArvoreB(nomeArquivoBinario, nomeArquivoIndex, numBuscas);
 
-            // faz as insercoes dos registros no arquivo de dados e no arquivo da arvore b
-            bool inseriu = inserirNovoDadoArvoreB(arquivoBin, arquivoArvB, numOperacoes);
-
-            // se o registro foi inserido, imprime o binario na tela dos arquivos de dados e da arvore b
+            // Se inserido com sucesso
             if(inseriu) {
-                binarioNaTela(arquivoBin);
-                binarioNaTela(arquivoArvB);
+                // Imprime os arquivos binários de dados e índice
+                binarioNaTela(nomeArquivoBinario);
+                binarioNaTela(nomeArquivoIndex);
             }
             break;
         }
